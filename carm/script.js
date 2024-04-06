0f8b34fb3b86057918669fa23ef750a749a1928f
fpserg <111433126+fpserg@users.noreply.github.com>
2024-04-06 12:05:21 +0300

    Update script.js

--- carm/script.js
+++ carm/script.js
@@ -1,3 +1,11 @@
+$.ajaxSetup({
+  async: false
+});
+
+let price
+
+addEventListener("load", function(e){
+
 let marketdata = "https://iss.moex.com/iss/engines/stock/markets/shares/boards/TQBR/securities/carm.json"
 
 try {
@@ -7,9 +15,10 @@ catch(err){
   alert(err)
   };
 
+document.getElementById("output").innerHTML = `<p>${price.toFixed(2)}</p>`
+})
+
 function pumpIt(){
   price += 0.01
   document.getElementById("output").innerHTML = `<p>${price.toFixed(2)}</p>`
 }
-
-document.getElementById("output").innerHTML = `<p>${price.toFixed(2)}</p>`
